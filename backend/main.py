5ea53a02ed89bc0c514f71f007e017cd89a0f09d
naethu <nathan.matzinger@gmail.com>
2024-10-19 10:22:20 +0200

    update to deploy in vercel

--- backend/main.py
+++ backend/main.py
@@ -8,7 +8,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://3050-wetterdaten-app.vercel.app/"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
