d76a372d3fd91592159160049a5c73c0a8795da0
naethu <nathan.matzinger@gmail.com>
2025-01-11 21:32:16 +0100

    version 5
    add api

--- api/index.py
+++ api/index.py
@@ -1,8 +1,62 @@
-from fastapi import FastAPI
+import json
+import os
 
-### Create FastAPI instance with custom docs and openapi url
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
+
 app = FastAPI(docs_url="/api/py/docs", openapi_url="/api/py/openapi.json")
 
-@app.get("/api/py/helloFastApi")
-def hello_fast_api():
-    return {"message": "Hello from FastAPI"}
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+def load_data():
+    json_file_path = os.path.join(os.path.dirname(__file__), "data", "meteodaten_2023_daily.json")
+    with open(json_file_path, "r") as file:
+        data = json.load(file)
+    return data
+
+
+data_cache = None
+
+
+def get_cached_data():
+    global data_cache
+    if data_cache is None:
+        data_cache = load_data()
+    return data_cache
+
+
+@app.get("/api/py/data")
+async def get_meteodaten():
+    try:
+        data = get_cached_data()
+        return JSONResponse(content=data)
+    except Exception as e:
+        return JSONResponse(content={"error": str(e)}, status_code=500)
+
+
+@app.get("/api/py/stations")
+def get_all_stations():
+    try:
+        data = get_cached_data()
+        stations_info = {}
+
+        for item in data:
+            if "Standortname" in item and "WGS84_lat" in item and "WGS84_lng" in item:
+                station_name = item["Standortname"]
+                if station_name not in stations_info:
+                    stations_info[station_name] = {
+                        "WGS84_lat": item["WGS84_lat"],
+                        "WGS84_lng": item["WGS84_lng"]
+                    }
+        stations = [{"Standortname": station, **coords} for station, coords in stations_info.items()]
+        return {"stations": stations}
+    except Exception as e:
+        return {"error": str(e)}
